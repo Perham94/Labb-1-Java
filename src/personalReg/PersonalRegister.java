54002f403c865feb9fd52780a34fbff977b763e0
chjunchi <chjunchi@chjunchi-U45JC>
2019-02-25 11:17:32 +0100

    Meny

--- src/personalReg/PersonalRegister.java
+++ src/personalReg/PersonalRegister.java
@@ -7,47 +7,49 @@ import java.util.ArrayList;
 
 public class PersonalRegister {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+start();
 	}
 	public static void start() throws IOException {
-		ArrayList<Personal> array = new ArrayList<>();
+		ArrayList<Personal> personalLista = new ArrayList<>();
 	
 		ConsoleReader cr = new ConsoleReader();
 
 		while (true) {
-			System.out.println("1: Avsluta");
-			System.out.println("2: Lägg till person");
-			System.out.println("3: Visa alla personer");
-			System.out.println("4: Läs in alla personer från fil");
-			System.out.println("5: Spara alla personer till fil");
-			System.out.println("6: Skapa en person automatiskt");
-			String s =cr.inStr(); 
-
-			switch (s) {
-				case "1" :
+			System.out.println("1: Lägg till personal");
+			System.out.println("2: Editera personal");
+			System.out.println("3: Radera personal");
+			System.out.println("4: Visa all personal");
+			System.out.println("5: Sök personal");
+			System.out.println("6: Sortera personal");			
+			System.out.println("7: Skapa en personal automatiskt");
+			System.out.println("8: Spara alla personal till fil");
+			System.out.println("9: Läs in alla personal från fil");
+			System.out.println("10: Avsluta");
+			int in =cr.inInt(); 
+			
+			switch (in) {
+				case 1 :
+					break;
+				case 2 :
+					break;
+				case 3 :
+					break;
+				case 4 :
+					break;
+				case 5 :
+					break;
+				case 6 :
+					return;
+				case 7 :
+					break;
+				case 8 :
+					break;
+				case 9 :
+					break;
+				case 10 :
 					System.out.println("Adjö!");
 					return;
-				case "2" :
-				
-					break;
-				case "3" :
-//					System.out.println("Personer: ");
-//					if (array!=null) {
-//						for (Personal p : array) {
-//							p.visa();
-//						}
-//					}
-					break;
-				case "4" :
-//					array = (ArrayList<Personal>) deSerialize(ser);
-//					System.out.println("Inläst!");
-					break;
-				case "5" :
-//					serialize(array, ser);
-//					System.out.println("Sparad!");
-					break;
-	
 			}
 		}
 	}
