f59865a0238078b567d60c345e552abaa4e80c57
chjunchi <chjunchi@chjunchi-U45JC>
2019-02-25 11:02:53 +0100

    Console reader

--- src/personalReg/ConsoleReader.java
+++ src/personalReg/ConsoleReader.java
@@ -1,5 +1,23 @@
 package personalReg;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class ConsoleReader {
+	public static String inStr() throws java.io.IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		return str;
+	}
+	public static int inInt() throws java.io.IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+
+		while (!str.matches("[0-9]+")) {
+			System.out.println("Not a valid number ");
+			str = br.readLine();
+		}
+		return Integer.parseInt(str);
+	}
 
 }
